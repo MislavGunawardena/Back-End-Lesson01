be020ceaaa04ddd4b7f12e1905dcde230de18872
Micheal Gunner <shakthigster@gmail.com>
2017-01-19 18:00:54 +0000

    Loan calculator - final version

--- lesson_02/loan_calculator.rb
+++ lesson_02/loan_calculator.rb
@@ -37,55 +37,63 @@ end
 
 prompt "Hello #{name}!"
 
-loan_amount = ''
-loop do # Get the loan amount
-  prompt 'Please enter the loan amount : '
-  loan_amount = gets.chomp
-  if verify_loan_amount(loan_amount)
-    break
-  else
-    prompt 'The loan amount you entered is not valid'
+loop do
+  loan_amount = ''
+  loop do # Get the loan amount
+    prompt 'Please enter the loan amount : '
+    loan_amount = gets.chomp
+    if verify_loan_amount(loan_amount)
+      break
+    else
+      prompt 'The loan amount you entered is not valid'
+    end
   end
+  
+  apr = ''
+  loop do # Get the APR
+    prompt 'Please enter the Annual Percentage Rate : '
+    apr = gets.chomp
+    if verify_apr(apr)
+      break
+    else
+      prompt 'The Annual Percentage Rate you entered is not valid.'
+    end
+  end
+  
+  duration_in_years = ''
+  loop do # Get duration in years
+    prompt 'Please enter the duration in years : '
+    duration_in_years = gets.chomp
+    if verify_duration(duration_in_years)
+      break
+    else
+      prompt 'The duration you entered is not valid.'
+    end
+  end
+  
+  # Calculating monthly interest
+  monthly_interest = (apr.to_f/100.0) / 12.0
+  
+  # Calculating duration in months
+  duration_in_months = duration_in_years.to_i * 12
+  
+  # Calculating monthly payment
+  monthly_payment =
+    calculate_monthly_payment(loan_amount.to_i,
+                              monthly_interest, duration_in_months)
+                              
+  prompt 'The monthly interest rate : '
+  prompt "#{monthly_interest*100}%"
+  
+  prompt 'Duration in months : '
+  prompt "#{duration_in_months}"
+  
+  prompt 'Monthly Payment : '
+  prompt "#{monthly_payment}"
+  
+  prompt 'Do you wish to make another calculation?(y/n)'
+  calculate_again = gets.chomp
+  break unless calculate_again.downcase.start_with?('y')
 end
 
-apr = ''
-loop do # Get the APR
-  prompt 'Please enter the Annual Percentage Rate : '
-  apr = gets.chomp
-  if verify_apr(apr)
-    break
-  else
-    prompt 'The Annual Percentage Rate you entered is not valid.'
-  end
-end
-
-duration_in_years = ''
-loop do # Get duration in years
-  prompt 'Please enter the duration in years : '
-  duration_in_years = gets.chomp
-  if verify_duration(duration_in_years)
-    break
-  else
-    prompt 'The duration you entered is not valid.'
-  end
-end
-
-# Calculating monthly interest
-monthly_interest = (apr.to_f/100.0) / 12.0
-
-# Calculating duration in months
-duration_in_months = duration_in_years.to_i * 12
-
-# Calculating monthly payment
-monthly_payment =
-  calculate_monthly_payment(loan_amount.to_i,
-                            monthly_interest, duration_in_months)
-                            
-prompt 'The monthly interest rate : '
-prompt "#{monthly_interest*100}%"
-
-prompt 'Duration in months : '
-prompt "#{duration_in_months}"
-
-prompt 'Monthly Payment : '
-prompt "#{monthly_payment}"
+prompt 'Thank you for using the loan calculator. Good Bye!'
