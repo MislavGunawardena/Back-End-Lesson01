0a78b33732c5db83c40875b69448a114a65e9cba
Micheal Gunner <shakthigster@gmail.com>
2017-01-30 23:40:00 +0000

    tic_tac_toe.rb

--- lesson_05/tic_tac_toe.rb
+++ lesson_05/tic_tac_toe.rb
@@ -1,9 +1,9 @@
 require 'pry'
-def display_board(positions)
+def display_board(board)
   puts '  1 2 3 '
   puts '  - - - '
-  
-  positions.each_with_index do |row, index|
+
+  board.each_with_index do |row, index|
     print "#{index + 1}"
     row.each do |cell|
       print "|#{cell}"
@@ -12,108 +12,156 @@ def display_board(positions)
   end
 end
 
-def player_input(positions)
-  row = nil
-  column = nil
-  loop do #Marking cell by player
-    loop do #Accepting row from player
-      puts 'Please enter a row number : '
-      row = gets.chomp.to_i
-      if [1,2,3].include?(row)
-        break
-      else
-        puts "That's not a valid row number. Please enter 1, 2, or 3:"
-      end
-    end
-    
-    loop do #Accepting column from player
-      puts 'Please enter a column number : '
-      column = gets.chomp.to_i
-      if [1,2,3].include?(column)
-        break
-      else
-        puts "That's not a valid column number. Please enter 1, 2, or 3:"
-      end
-    end
-    
-    if positions[row-1][column-1] == ' '
-      positions[row-1][column-1] = :x
+def available_rows(board)
+  [1,2,3].select do |number|
+    board[number - 1].any? { |cell| cell == ' ' }
+  end
+end
+
+def numbers_in_array(arr)
+  last_number = arr.pop
+  if arr.size > 1
+    "#{arr.join(', ')}, or #{last_number}"
+  elsif arr.size == 1
+    "#{arr[0]} or #{last_number}"
+  else
+    "#{last_number}"
+  end
+end
+
+def player_chooses_row(cell, board)
+  row = ''
+  loop do
+    puts "Please enter a row number : #{numbers_in_array(available_rows(board))}."
+    row = gets.chomp.to_i
+    if available_rows(board).include?(row)
       break
     else
-      display_board(positions)
-      puts "The cell (#{row}#{column}) have already been marked. Please choose another cell"
+      puts "That's not a valid row number. "
+    end
+  end
+  cell[0] = row
+end
+
+def player_chooses_column(cell)
+  column = ''
+  loop do
+    puts 'Please enter a column number : '
+    column = gets.chomp.to_i
+    if [1, 2, 3].include?(column)
+      break
+    else
+      puts "That's not a valid column number. Please enter 1, 2, or 3:"
+    end
+  end
+  cell[1] = column
+end
+
+def player_input(board)
+  loop do
+    marked_cell = ['', '']
+    player_chooses_row(marked_cell, board)
+    player_chooses_column(marked_cell)
+
+    if board[marked_cell[0] - 1][marked_cell[1] - 1] == ' '
+      board[marked_cell[0] - 1][marked_cell[1] - 1] = 'x'
+      break
+    else
+      display_board(board)
+      puts "The cell (#{marked_cell[0]}#{marked_cell[1]}) " +
+           'has already been marked. Please choose another cell'
     end
   end
 end
 
-def computer_turn(positions)
-  open_positions =
-    positions.map do |row|
-      row.select {|cell| cell == ' '}
+def computer_turn(board)
+  open_cells = 
+    board.flatten.select do |cell|
+      cell == ' '
     end
-  open_positions.reject! do |row|
-    row == []
-  end
-    
-  #binding.pry
-  open_positions.sample.sample.replace('o')
+  open_cells.sample.replace('o')
 end
 
-def valid_pattern?(mark, positions)
-  positions.each do |row|
-    return true if row.all? {|cell| cell == mark}
-  end
+def set_of_rows(board)
+  board
+end
 
-  3.times do |c|
+def set_of_columns(board)
+  all_columns = []
+  3.times do |counter1|
     column = []
-    3.times do |r|
-      column << positions[r][c]
+    3.times do |counter2|
+      column << board[counter2][counter1]
     end
-    return true if column.all?{|cell| cell == mark}
+    all_columns << column
   end
-
-  first_diagonal =
-    [0,1,2].map {|number| positions[number][number]}
-  return true if first_diagonal.all? {|cell| cell == mark}
-    
-  second_diagonal = 
-    [0,1,2].map {|number| positions[2-number][number]}
-  return true if second_diagonal.all?{|cell| cell == mark}
-
-  return false
+  
+  all_columns
 end
 
-def winner(positions) #returns the winner
-  if valid_pattern?(:x, positions)
-    :player
-  elsif valid_pattern?('o', positions)
-    :computer
-  elsif board_full?(positions)
-    :draw
+def set_of_diagonals(board)
+  all_diagonals = []
+  
+  diagonal_1 = []
+  diagonal_2 = []
+  3.times do |counter|
+    diagonal_1 << board[counter][counter] 
+    diagonal_2 << board[2-counter][counter]
+  end
+  all_diagonals.push(diagonal_1, diagonal_2)
+  
+  all_diagonals
+end
+
+def winning_sequences(board)
+  winning_combinations = []
+  
+  winning_combinations += set_of_rows(board)
+  winning_combinations += set_of_columns(board)
+  winning_combinations += set_of_diagonals(board)
+  
+  winning_combinations
+end
+
+def player_won?(board)
+  winning_sequences(board).any? do |arr|
+    arr.all? { |cell| (cell == 'x') }
+  end
+end
+
+def computer_won?(board)
+  winning_sequences(board).any? do |arr|
+    arr.all? { |cell| (cell == 'o') }
+  end
+end
+
+def board_full?(board)
+  board.flatten.all? { |cell| cell != ' '}
+end
+
+def result(board)
+  if player_won?(board)
+    'player'
+  elsif computer_won?(board)
+    'computer'
+  elsif board_full?(board)
+    'tie'
   else
     nil
   end
 end
 
-def board_full?(positions)
-  open_positions =
-    positions.map do |row|
-      row.select {|cell| cell == ' '}
-    end
-  open_positions.reject! do |row|
-    row == []
-  end
-  
-  (open_positions == []) ? true : false
+def game_over?(board)
+  !!result(board)
 end
 
-def display_result(positions)
-  if winner(positions) == :player
+def display_result(board)
+  if result(board) == 'player'
     puts 'You won!'
-  elsif winner(positions) == :computer
+  elsif result(board) == 'computer'
     puts 'The computer won!'
-  elsif winner(positions) == :draw
-    puts "It's a draw!"
+  elsif result(board) == 'tie'
+    puts "It's a tie!"
   else
     nil
   end
@@ -122,23 +170,23 @@ end
 puts 'Welcome to the tic-tac-toe game!'
 
 loop do
-  positions = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
-  display_board(positions)
-  
+  board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+  display_board(board)
+
   loop do
-    player_input(positions)
-    display_board(positions)
-    break if winner(positions)
-    
+    player_input(board)
+    display_board(board)
+    break if game_over?(board)
+
     puts "Please press enter for the computer to play it's turn."
     gets
-    computer_turn(positions)
-    display_board(positions)
-    break if winner(positions)
+    computer_turn(board)
+    display_board(board)
+    break if game_over?(board)
   end
-  
-  display_result(positions)
-  
+
+  display_result(board)
+
   another_game = ''
   loop do
     puts 'Do you want to play again? (y/n)'
@@ -154,4 +202,3 @@ loop do
 end
 
 puts "Thank you for playing tic-tac-toe. Have a good day!"
-
