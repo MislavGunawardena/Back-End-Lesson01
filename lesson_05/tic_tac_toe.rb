33979b2f233dd6e1b08965bfd3c95ca909702405
Micheal Gunner <shakthigster@gmail.com>
2017-02-04 06:59:57 +0000

    tic_tac_toe.rb: Defined 2 helper functions, tournament_over? and display_tournament_result, which are called in play_tournament.

--- lesson_05/tic_tac_toe.rb
+++ lesson_05/tic_tac_toe.rb
@@ -228,18 +228,22 @@ def play_tournament(brd, score, first_turn)
   display_result(brd)
   press_enter_to_continue
   
-  if score[:player] == 5
-    prompt "You won the tournament!"
-    return
-  elsif score[:computer] == 5
-    prompt "The computer won the tournament!"
-    return
-  end
+  display_tournament_result(score) if tournament_over?(score)
+  return if tournament_over?(score)
   
   brd = empty_board
   display_game(brd, score)
   switch_turn(first_turn)
   play_tournament(brd, score, first_turn)
+end
+
+def tournament_over?(score)
+  [score[:player], score[:computer?]].include?(5)
+end
+
+def display_tournament_result(score)
+  prompt "You won the tournament!" if score[:player] == 5
+  prompt "The computer won the tournament!" if score[:computer] == 5
 end
 
 prompt 'Welcome to the tic-tac-toe game!'
