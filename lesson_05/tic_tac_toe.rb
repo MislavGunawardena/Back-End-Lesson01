0a832d627e606c59fa298a6d13e5cbd644843763
Micheal Gunner <shakthigster@gmail.com>
2017-02-08 07:10:28 +0000

    tic_tac_toe.rb: changed the tournament winning score to 5

--- lesson_05/tic_tac_toe.rb
+++ lesson_05/tic_tac_toe.rb
@@ -5,7 +5,7 @@ INITIAL_TURN = :choose
 WINNING_SEQUENCES = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
                      [1, 4, 7], [2, 5, 8], [3, 6, 9],
                      [1, 5, 9], [7, 5, 3]].freeze
-TOURNAMENT_WINNING_SCORE = 2
+TOURNAMENT_WINNING_SCORE = 5
 
 def prompt(msg)
   puts "=> #{msg}"
