ec15d61da0aa982c209f0aa81abba12198ed55d3
Micheal Gunner <shakthigster@gmail.com>
2017-02-09 10:08:03 +0000

    twenty_one.rb: rubocop

--- lesson_05/twenty_one.rb
+++ lesson_05/twenty_one.rb
@@ -16,11 +16,11 @@ end
 def total(cards)
   sum = 0
   values = cards.map { |card| card[1] }
-  
+
   values.each do |value|
     if value == 'A'
       sum += 11
-    elsif value.to_i == 0
+    elsif value.to_i.zero?
       sum += 10
     else
       sum += value.to_i
